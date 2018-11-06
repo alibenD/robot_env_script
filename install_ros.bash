45104f27a63a76dc2cbad953e74c75d1578897f4
alibenD <aliben.develop@gmail.com>
2018-11-06 18:22:07 +0800

    Initial commit

---
+++ install_ros.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#---Automatically Generated from template 'bash' wrote by @aliben---
+# @Copyright (C) 2018 All rights reserved.
+# @file: install_ros.bash
+# @author: aliben.develop@gmail.com
+# @created_date: 2018-11-06 14:33:45
+# @last_modified_date: 2018-11-06 15:50:48
+# @brief: TODO
+# @details: TODO
+#---***********************************************---
+
+
+#---Variables
+CREATED_TIME=`date '+%Y-%m-%d %H:%M:%S'`
+CREATED_YEAR=`date '+%Y'`
+
+#---Shell Command
+sudo apt update && sudo apt upgrade
+sudo apt install -y \
+build_essential \
+git \
+python-rosinstall \
+python-rosinstall-generator \
+python-wstool \
+build-essential \
+vim \
+ctags \
+libopencv-* \
+
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt update
+sudo apt -y install ros-melodic-desktop-full \
+                    ros-melodic-map* \
+                    ros-melodic-joy* \
+
+sudo rosdep init
+sudo rosdep update
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+echo "source $HOME/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
