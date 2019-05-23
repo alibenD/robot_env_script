f1c5f2e3f9c5f927d117cb3501ac59512a6e7a70
alibenD <aliben.develop@gmail.com>
2019-05-23 18:45:04 +0800

    Updated ROS

--- install_ros.bash
+++ install_ros.bash
@@ -5,7 +5,7 @@
 # @file: install_ros.bash
 # @author: aliben.develop@gmail.com
 # @created_date: 2018-11-06 14:33:45
-# @last_modified_date: 2019-05-23 16:54:20
+# @last_modified_date: 2019-05-23 18:44:45
 # @brief: TODO
 # @details: TODO
 #---***********************************************---
@@ -19,7 +19,10 @@ CREATED_YEAR=`date '+%Y'`
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
 sudo apt update
-sudo apt -y install ros-melodic-desktop-full \
+sudo apt -y install python-catkin-pkg-modules\
+                    python-rosdistro-modules\
+                    python-rospkg-modules\
+                    ros-melodic-desktop-full \
                     ros-melodic-map* \
                     ros-melodic-joy* \
                     ros-melodic-tf*
